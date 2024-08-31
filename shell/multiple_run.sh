4ef91c73f4f5bd776747b8e43f9ce23fcf78ab89
sinchir0 <dekunattou@gmail.com>
2024-08-31 01:58:50 +0000

    add

--- shell/multiple_run.sh
+++ shell/multiple_run.sh
@@ -4,8 +4,9 @@
 # ./shell/do_notebook_by_cli.sh exp/e032.ipynb
 # ./shell/do_notebook_by_cli.sh exp/e033.ipynb
 # ./shell/do_notebook_by_cli.sh exp/e034.ipynb
-./shell/do_notebook_by_cli.sh exp/e037.ipynb
-./shell/do_notebook_by_cli.sh exp/e038.ipynb
-./shell/do_notebook_by_cli.sh exp/e039.ipynb
-./shell/do_notebook_by_cli.sh exp/e010.ipynb
-./shell/do_notebook_by_cli.sh exp/e011.ipynb
+# ./shell/do_notebook_by_cli.sh exp/e037.ipynb
+# ./shell/do_notebook_by_cli.sh exp/e038.ipynb
+# ./shell/do_notebook_by_cli.sh exp/e039.ipynb
+# ./shell/do_notebook_by_cli.sh exp/e010.ipynb
+# ./shell/do_notebook_by_cli.sh exp/e011.ipynb
+./shell/do_notebook_by_cli.sh exp/e040.ipynb
