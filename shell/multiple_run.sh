b7c1f05335cf03ede707ea866ff8828ed4427908
sinchir0 <dekunattou@gmail.com>
2024-09-03 00:49:03 +0000

    add

--- shell/multiple_run.sh
+++ shell/multiple_run.sh
@@ -3,4 +3,4 @@
 # ./shell/do_notebook_by_cli.sh exp/e042.ipynb
 # ./shell/do_notebook_by_cli.sh exp/e043.ipynb
 # ./shell/do_notebook_by_cli.sh exp/e044.ipynb
-./shell/do_notebook_by_cli.sh exp/e046.ipynb
+./shell/do_notebook_by_cli.sh exp/late-e002.ipynb
